6f99042d707c91801dd26b003bb58de300182850
Sarthak Tandon <sarthak0120@gmail.com>
2018-09-29 01:33:05 +0530

    Integrated script to node app, iteration #1

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require("express"),
+    bodyParser= require("body-parser"),
+    app       = express();
+const {PythonShell} = require("python-shell");
+var sys = require('sys');
+var spawn = require("child_process").spawn;
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res){
+ res.render("home")
+});
+app.post('/k', function(req, res) {
+   
+  var options = {
+  mode: 'text',
+ // pythonPath: "C:/Users/hp/Anaconda3-2/python.exe",
+  pythonPath:"C:/Users/sarth/AppData/Local/Continuum/anaconda3/python.exe",
+  pythonOptions: ['-u'],
+  scriptPath: './',
+  args: ['-p','shape_predictor_68_face_landmarks.dat']
+};
+
+PythonShell.run('main_drowsy.py', options, function (err, message) {
+    if(err)
+      throw err;
+    console.log(message);
+    if(message=='1')
+    {
+        //alarm'
+        res.send("OH LOL!");
+    }
+ // res.render("result", {message: message, claim: claim, says:says});
+// console.log("hii");
+
+});
+
+});
+app.listen(3000, "localhost", function(err){
+    console.log("Server on duty, Mallady!");
+});
--- main_drowsy.py
+++ main_drowsy.py
@@ -1,5 +1,4 @@
-##imports
-from scipy.spatial import distance as dist
+from scipy.spatial import distance
 from imutils.video import VideoStream
 from imutils import face_utils
 import argparse
@@ -7,13 +6,10 @@ import imutils
 import dlib
 import cv2
 
-# Detect Blinks
-
 def eye_aspect_ratio(eye):
-  A = dist.euclidean(eye[1], eye[5])
-  B = dist.euclidean(eye[2], eye[4])
-  
-  C = dist.euclidean(eye[0], eye[3])
+  A = distance.euclidean(eye[1], eye[5])
+  B = distance.euclidean(eye[2], eye[4])  
+  C = distance.euclidean(eye[0], eye[3])
   ear = (A+B) / (2.0 * C)
   return ear
 
@@ -50,7 +46,7 @@ while True:
   w=0
   h=0
   for (mx, my, mw, mh) in face:
-    d = dist.euclidean((mx+w/2, my+h/2), (width/2, height/2))
+    d = distance.euclidean((mx+w/2, my+h/2), (width/2, height/2))
     if(d < min_dis):
       min_dis = d
       x = mx
@@ -58,24 +54,16 @@ while True:
       w = mw
       h = mh
 
-  # detect faces in the grayscale frame
   rects = detector(gray, 0)
   prevcount = 0
-  # loop over the face detections
   for rect in rects:
-    # determine the facial landmarks for the face region, then
-    # convert the facial landmark (x, y)-coordinates to a NumPy
-    # array
     shape = predictor(gray, rect)
     shape = face_utils.shape_to_np(shape)
-    # extract the left and right eye coordinates, then use the
-    # coordinates to compute the eye aspect ratio for both eyes
     leftEye = shape[lStart:lEnd]
     rightEye = shape[rStart:rEnd]
     leftEAR = eye_aspect_ratio(leftEye)
     rightEAR = eye_aspect_ratio(rightEye)
     danger = 0
-    # average the eye aspect ratio together for both eyes\ 
     ear = (leftEAR + rightEAR) / 2.0
     leftEyeHull = cv2.convexHull(leftEye)
     rightEyeHull = cv2.convexHull(rightEye)
@@ -85,8 +73,9 @@ while True:
     if (leftEAR < EYE_AR_THRESH - 0.12 and rightEAR < EYE_AR_THRESH - 0.12):
       print("Both Eyes Blinked")
       continuous_counter = continuous_counter + 1
-      if(continuous_counter>7):
+      if(continuous_counter>5):
         danger = 1
+        print("Danger")
       COUNTER += 1
       TOTAL += 1    
       prevcount = COUNTER
@@ -94,27 +83,16 @@ while True:
     else:
       continuous_counter = 0
     
-    cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.putText(frame, "Left: {:.2f}".format(leftEAR), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.putText(frame, "Right: {:.2f}".format(rightEAR), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.putText(frame, "DANGER!!: {:.2f}".format(danger), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(frame, "Blinks count: {}".format(TOTAL), (10, 30), cv2.FONT_ITALIC, 0.7, (0, 255, 0), 2)
+    cv2.putText(frame, "DANGER!!: {:.2f}".format(danger), (10, 90), cv2.FONT_ITALIC, 0.7, (0, 255, 0), 2)
+    cv2.putText(frame, "Aspect ratio: {:.2f}".format(ear), (300, 30), cv2.FONT_ITALIC, 0.7, (0, 255, 0), 2)
+
     break
 
   cv2.imshow("Frame", frame)
   key = cv2.waitKey(1) & 0xFF
-
-  # if the `q` key was pressed, break from the loop
   if key == ord("q"):
     break
 
 cv2.destroyAllWindows()
 vs.stop()
-                                                 
-                                                  
-                                                  
-                                            
-
-
-
-

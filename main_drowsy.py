45e847e1ea0f833ed063e1d6a37eee883a48970f
Sarthak Tandon <sarthak0120@gmail.com>
2018-09-28 20:00:17 +0530

    Added frame looping

--- main_drowsy.py
+++ main_drowsy.py
@@ -43,7 +43,63 @@ time.sleep(1.0)
 currentCount = 0
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+while True:
+  frame = vs.read()
+  frame = imutils.resize(frame, width=450)
+  height, width, c = frame.shape
+
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  face = face_cascade.detectMultiScale(gray, 1.15)
+
+  min_dis = 100000
+  x=0
+  y=0
+  w=0
+  h=0
+  for (mx, my, mw, mh) in face:
+    d = dist.euclidean((mx+w/2, my+h/2), (width/2, height/2))
+    if(d < min_dis):
+      min_dis = d
+      x = mx
+      y = my
+      w = mw
+      h = mh
+
+  # detect faces in the grayscale frame
+  rects = detector(gray, 0)
+  prevcount = 0
+  # loop over the face detections
+  for rect in rects:
+    # determine the facial landmarks for the face region, then
+    # convert the facial landmark (x, y)-coordinates to a NumPy
+    # array
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+    # extract the left and right eye coordinates, then use the
+    # coordinates to compute the eye aspect ratio for both eyes
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+    danger = 0
+    # average the eye aspect ratio together for both eyes\ 
+    ear = (leftEAR + rightEAR) / 2.0
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [rightEyeHull],  -1, (0, 255, 0), 1)
                                                   
+    if (leftEAR < EYE_AR_THRESH - 0.12 and rightEAR < EYE_AR_THRESH - 0.12):
+      print("Both Eyes Blinked")
+      continuous_counter = continuous_counter + 1
+      if(continuous_counter>7):
+        danger = 1
+      COUNTER += 1
+      TOTAL += 1    
+      prevcount = COUNTER
+            
+    else:
+      continuous_counter = 0
                                                   
                                                   
                                                   

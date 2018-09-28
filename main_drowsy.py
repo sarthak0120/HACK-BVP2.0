fa5fc768d8b7b7c79eccf7329e08b0acef2783e3
Sarthak Tandon <sarthak0120@gmail.com>
2018-09-28 18:08:46 +0530

    Update main_drowsy.py

--- main_drowsy.py
+++ main_drowsy.py
@@ -21,5 +21,35 @@ def eye_aspect_ratio(eye):
   ear = (A+B) / (2.0 * C)
   return ear
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", help = "path to facial landmark predictor")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+COUNTER = 0
+TOTAL = 0
+
+print("Loading Facial Landmark Predictor....")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS[["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS[["right_eye"]
+                                                 
+print("Starting Live Video Stream...")
+vs = VideoStream(src = 0).start()
+fileStream = False
+time.sleep(1.0)
+currentCount = 0
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                            
+
+
 
 
